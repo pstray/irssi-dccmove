27068ac951785981474e439d802b60d81ef4e1a3
Peder Stray <peder.stray@gmail.com>
2003-05-22 21:24:32 +0200

    Add check for completeness, don't move uncomplete files

--- dccmove.pl
+++ dccmove.pl
@@ -4,15 +4,15 @@
 
 use Irssi;
 use vars qw{$VERSION %IRSSI};
-($VERSION) = '$Revision: 1.1 $' =~ / (\d+\.\d+) /;
+($VERSION) = '$Revision: 1.2 $' =~ / (\d+\.\d+) /;
 %IRSSI = (
-          name        => 'dccmove',
-          authors     => 'Peder Stray',
-          contact     => 'peder@ninja.no',
-          url         => 'http://ninja.no/irssi/dccmove.pl',
-          license     => 'GPL',
-          description => 'Move completed dcc gets to the subfolder done',
-         );
+	  name        => 'dccmove',
+	  authors     => 'Peder Stray',
+	  contact     => 'peder@ninja.no',
+	  url         => 'http://ninja.no/irssi/dccmove.pl',
+	  license     => 'GPL',
+	  description => 'Move completed dcc gets to the subfolder done',
+	 );
 
 sub sig_dcc_closed {
     my($dcc) = @_;
@@ -24,11 +24,19 @@ sub sig_dcc_closed {
     ($dir,$file) = $dcc->{file} =~ m,(.*)/(.*),;
     $dir .= "/done";
 
+    if ($dcc->{transfd} < $dcc->{size}) {
+	printf('%%gDCC aborted %%_%s%%_, %%R%d%%%%%%g remaining%%n',
+	       $file,
+	       $dcc->{size} ? 100 - $dcc->{transfd}/$dcc->{size}*100 : 0,
+	      );
+	return;
+    }
+
     mkdir $dir, 0755 unless -d $dir;
     rename $dcc->{file}, "$dir/$file";
 
-    print '%gDCC moved %_'.$file.'%_ to %_'.$dir.'%_%n';
-     
+    printf('%%gDCC moved %%_%s%%_ to %%_%s%%_%%n', $file, $dir);
+
 }
 
 Irssi::signal_add('dcc closed', 'sig_dcc_closed');
